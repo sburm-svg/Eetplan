43d9451aa527746aa7e49a37e6d84e80c58c69c2
sburm-svg <stijn.burmanje@gmail.com>
2026-06-21 16:37:16 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect, useCallback, useMemo } from "react";
-
+import { recipes } from "./data/recipes"
 /* ─────────────────────────────────────────────
    DESIGN TOKENS
 ───────────────────────────────────────────── */
